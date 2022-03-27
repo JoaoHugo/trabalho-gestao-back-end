c4eb2e81fb8c0771eea3cf068fb766ac5b942430
João Hugo <joaohmarinho99@gmail.com>
2022-03-27 19:21:15 -0300

    Anotação para liberar o CORS

--- src/main/java/com/hostel/controller/GuestController.java
+++ src/main/java/com/hostel/controller/GuestController.java
@@ -8,6 +8,7 @@ import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.hostel.dto.GuestDto;
 import com.hostel.exception.ExistingGuestException;
 import com.hostel.service.GuestService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "guests")
 public class GuestController {
